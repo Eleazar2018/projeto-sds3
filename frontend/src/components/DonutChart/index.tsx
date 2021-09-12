069cf81bc66bf329b2eecb12768b104108a9f457
Eleazar Ribeiro <eliribeiro2010@gmail.com>
2021-09-12 15:29:42 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -9,26 +9,25 @@ type ChartData = {
     series: number[];
 }
 const DonutChart = () => {
-    
-    const [chartData, setChartData] = useState<ChartData>({ labels: [], series:[]});
+
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
     //Forma errada
-   // let chartData: ChartData = { labels: [], series: [] };
-    
+    // let chartData: ChartData = { labels: [], series: [] };
+
     useEffect(() => {
         axios.get(`${BASE_URL}/sales/amount-by-seller`)
-    .then((response) => {
-        const data = response.data as SaleSum[];
-        const myLabels = data.map(x => x.sellerName);
-        const mySeries = data.map(x => x.sum);
+            .then((response) => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
 
-        setChartData({ labels: myLabels, series: mySeries});
-        console.log(chartData);
-    });
+                setChartData({ labels: myLabels, series: mySeries });
+            });
     }, []);
 
     //Forma errada    
-    
+
     //const mockData = {
     //    series: [477138, 499928, 444867, 220426, 473088],
     //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
